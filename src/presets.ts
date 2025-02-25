edb3bdc03a7bcff9655112777784eb2509a5cb25
Fa1k3n <johan.falkenstrom@gmail.com>
2025-02-25 09:18:54 +0100

    missed updating the version tag and add preset for recording

--- src/presets.ts
+++ src/presets.ts
@@ -8,6 +8,7 @@ import { MacroPresets } from './functions/macro'
 import { SuperSourcePresets } from './functions/superSource'
 import { PlaybackPresets } from './functions/playback'
 import { UpstreamKeyerPresets } from './functions/upstreamKeyer'
+import { RecordPresets } from './functions/record'
 import { SettingsPresets } from './functions/settings'
 
 export function presets(instance: GoStreamInstance): CompanionPresetDefinitions {
@@ -19,6 +20,7 @@ export function presets(instance: GoStreamInstance): CompanionPresetDefinitions 
 		...MacroPresets.create(instance.model),
 		...SuperSourcePresets.create(instance.model),
 		...PlaybackPresets.create(instance.model),
+		...RecordPresets.create(instance.model),
 		...UpstreamKeyerPresets.create(instance.model),
 		...SettingsPresets.create(instance.model),
 	}
